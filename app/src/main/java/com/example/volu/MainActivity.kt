bf7579ab2b0fb6ea4b70424aec1a517ef22309b2
oscarnipps <oscarnips@gmail.com>
2021-09-08 15:07:17 +0100

    initial commit with dependencies

---
+++ app/src/main/java/com/example/volu/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.volu
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var name = "oscar"
+    }
+}
